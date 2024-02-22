b702a8c5a4993b5f8e4b612c74fa92daea50f53c
glaskula <42672254+glaskula@users.noreply.github.com>
2024-02-22 18:16:00 +0100

    Fixed 4 posters

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
     <header className="App-header">
-      <h1>My Projects</h1>
+      <h1>Recent Projects</h1>
       <div className="projects-container">
         <img src={`${process.env.PUBLIC_URL}/FilmFriends.png`} alt="project-image" className="project-image"/>
         <img src={`${process.env.PUBLIC_URL}/Frogic.png`} alt="project-image" className="project-image"/>
+        <img src={`${process.env.PUBLIC_URL}/Table Poster-1.png`} alt="project-image" className="project-image"/>
+        <img src={`${process.env.PUBLIC_URL}/IXDPOSTER2.png`} alt="project-image" className="project-image"/>
       </div>
     </header>
   </div>
